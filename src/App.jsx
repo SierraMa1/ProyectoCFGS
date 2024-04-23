9430b0165f3d32e91709234bb13a741e979827e1
SierraMa1 <mariasierrasanchez1990@gmail.com>
2024-04-23 15:48:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    
+      <div className="h-12 max-w-[1440px] border px-[150px] shadow-md">
+        <p className="text-blue-500 text-3xl font-bold">ElectroFinder</p>
+      </div>
+      <div className="grid h-screen max-w-[1440px] grid-cols-3 gap-[30px] px-[150px]">
+        <div className="gap-4 py-8">
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className=" font-bold">Electricista Luca</p>
+            <p className=" text-gray-500">Málaga</p>
+            <br/>
+            <p className=" text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+        </div>
+        <div className="gap-4 py-8">
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+        </div>
+        <div className="gap-4 py-8">
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+          <div className="py-4 rounded-xl border shadow-md">
+            <p className="font-bold">Electricista Luca</p>
+            <p className="text-gray-500">Málaga</p>
+            <br/>
+            <p className="text-gray-900">Otros servicios....</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
